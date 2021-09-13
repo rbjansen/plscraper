0d3a4318c8cf50670a21975a38043516b40a77ee
rbjansen <r.b.jansen.uu@gmail.com>
2021-09-13 10:06:54 +0200

    initial scraper

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # plscraper
+
+Scrapes country profiles at https://www.policinglaw.info/ to markdown-pdf.
+
+## Installation and usage
+
+First, `cd` into this project's directory and set up a virtual environment by running the following in your command line:
+```
+virtualenv plscraper
+```
+Activate your virtual environment with:
+```
+source plscraper/bin/activate
+```
+Install the dependencies with:
+```
+pip install -r requirements.txt
+```
+You'll also need Pandoc for the document conversion. Assuming you're on macOS, get it through homebrew:
+```
+brew install pandoc
+```
+After this you should be good to go! Run the scraper with:
+```
+python scraper.py
+```
+It will output each country's profile page to pdf under /outputs.
---
+++ scraper.py
@@ -0,0 +1,61 @@
+"""
+Scrapes country profiles at https://www.policinglaw.info/ to markdown-pdf.
+"""
+
+import os
+import tempfile
+import subprocess
+from typing import List
+import requests
+from bs4 import BeautifulSoup
+from markdownify import markdownify
+from alive_progress import alive_it
+
+BASE_URL = "https://www.policinglaw.info/"
+
+
+def collect_country_urls(url: str) -> List[str]:
+    """Collects all country URLs."""
+    req = requests.get(url=url)
+    soup = BeautifulSoup(req.content, "html.parser")
+    urls = []
+    dropdown = soup.find("ul", class_="dropdown-menu country-dropdown")
+    for li in dropdown.find_all("li"):
+        urls.append(li.a.get("href"))
+    return urls
+
+
+def compile_pdfs(country_urls: List[str], path: str) -> None:
+    """Compiles page to markdown and pdf."""
+    for url in alive_it(country_urls):
+        name = url.split("/")[-2]
+        req = requests.get(url=url)
+        soup = BeautifulSoup(req.content, "html.parser")
+        header = soup.find("div", class_="pb30")
+        body = soup.find("div", class_="main-column")
+        fpath = os.path.join(path, f"{name}.pdf")
+        with tempfile.TemporaryDirectory() as tempdir:
+            mdpath = os.path.join(tempdir, f"{name}.md")
+            with open(mdpath, "w") as f:
+                f.write(markdownify(str(header), strip=["img"]))
+                f.write(
+                    markdownify(str(body)).replace(
+                        "/assets", "https://www.policinglaw.info/assets"
+                    )
+                )  # Add original url to preserve download links.
+            subprocess.run(
+                [f"pandoc --pdf-engine=xelatex {mdpath} -o {fpath}"],
+                shell=True,
+            )
+
+
+def main():
+    output_path = "./output"
+    if not os.path.exists(output_path):
+        os.mkdir(output_path)
+    urls = collect_country_urls(BASE_URL)
+    compile_pdfs(urls, output_path)
+
+
+if __name__ == "__main__":
+    main()
